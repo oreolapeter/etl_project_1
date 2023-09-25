85f97496bcbcee01902baa5bbb80f48582a08af1
oreolapeter <oreolapeter@gmail.com>
2023-09-25 06:04:53 -0700

    feature(modifications): the modification to the helper file to conceal my aws access key

--- utils/helper.py
+++ utils/helper.py
@@ -1,11 +1,26 @@
 import boto3
 
-access_key_ID = 'AKIAVFIGTUJDIWWJPGMA'		
-Secret_key = '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'
+from configparser import ConfigParser
 
-region = 'eu-west-1'
+config = ConfigParser()
 
-bucket_name = 'payminutelekan'
+config.read('.env')
+
+region = config['AWS']['region']
+
+access_key_ID = config['AWS']['access_key_ID']		
+
+Secret_key = config['AWS']['Secret_key']
+
+bucket_name = config['AWS']['bucket_name']
+
+
+access_key_ID = access_key_ID		
+Secret_key = Secret_key
+
+region = region
+
+bucket_name = bucket_name
 
 def create_bucket():
     
